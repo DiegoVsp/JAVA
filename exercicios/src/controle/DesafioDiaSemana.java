d7e7d6bf4d7f1f874ab8da9c1cdf6ecea3af9945
DiegoVsp <diego-vespa2020@outlook.com.br>
2020-12-28 21:29:56 -0300

    att aula Java

---
+++ exercicios/src/controle/DesafioDiaSemana.java
@@ -0,0 +1,35 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DesafioDiaSemana {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Digite o nome do dia da semana: ");
+		String diaSem = entrada.next();
+
+		//Não use == com String
+		if (diaSem.equalsIgnoreCase("Domingo")) {
+			System.out.println("1");
+		} else if (diaSem.equalsIgnoreCase("Segunda")) {
+			System.out.println("2");
+		} else if (diaSem.equalsIgnoreCase("Terça")) {
+			System.out.println("3");
+		} else if (diaSem.equalsIgnoreCase("Quarta")) {
+			System.out.println("4");
+		} else if (diaSem.equalsIgnoreCase("Quinta")) {
+			System.out.println("5");
+		} else if (diaSem.equalsIgnoreCase("Sexta")) {
+			System.out.println("6");
+		} else if (diaSem.equalsIgnoreCase("Sabado")) {
+			System.out.println("7");
+		} else {
+			System.out.println("Dia Inválido");
+		}
+
+		entrada.close();
+	}
+}
--- exercicios/src/controle/DesafioIf.java
+++ exercicios/src/controle/DesafioIf.java
@@ -5,7 +5,7 @@ public class DesafioIf {
 	public static void main(String[] args) {
 		double nota = 1.3;
 
-		//não usar ; em estruturas de controle (tem uma exceção)
+		//não usar ; em estruturas de controle (tem uma exceção )
 		if (nota >= 9.0)
 		{
 			System.out.println("Quadro de Honra!");
